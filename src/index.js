d7b83d4acee441f5a0fc1c564a3b7692d62c39eb
kornkanok <kornkanok.lieng@gmail.com>
2021-06-07 22:13:22 +0700

    change route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { store, persistor } from './redux/store'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
@@ -9,11 +9,11 @@ import App from './App'
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
   ,document.getElementById('root')
 )
